153344a5137b19d7dcb669c93d0465fba667b205
Makhluk Tuhan Paling Seksi <virdi@l-essential.com>
2024-03-12 13:58:39 +0700

    Fix name

--- app/pv/page.tsx
+++ app/pv/page.tsx
@@ -4,7 +4,7 @@ import { BtnColl } from '@/app/ui/btn-collection';
 export default function PresentValue() {
     return (
         <main className="flex min-h-screen flex-col p-6">
-            <label className="block mb-2 text-sm font-medium">Kalkulator Cost of Capital</label>
+            <label className="block mb-2 text-sm font-medium">Present Value</label>
             <BtnColl />
             <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
                 <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:px-20">
